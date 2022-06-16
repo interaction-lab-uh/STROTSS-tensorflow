574a2ae1fbe5a8740e690e7947693fb763009ac7
ppza53893 <private.ukyo.info@gmail.com>
2022-06-16 18:52:01 +0900

    _request_vgg_model -> tf.keras.utils.get_file

--- nn/model.py
+++ nn/model.py
@@ -1,42 +1,38 @@
-import logging
+from typing import Optional
 
-import requests
 import tensorflow as tf
 
-logger = logging.getLogger('STROTSS')
 
-
-def _request_vgg_model(filename: str):
-    try:
-        logger.info(f'Downloading {filename}...')
-        result = requests.get(f'https://ppza53893.github.io/box/weights/{filename}')
-        with open(f'./{filename}', 'wb') as f:
-            f.write(result.content)
-        del result
-    except:
-        raise Exception(
-            f'Unable to download {filename}. '
-            f'Please download directly from "https://ppza53893.github.io/box/weights/{filename}".') 
-
+_URL = 'https://ppza53893.github.io/box/weights/{filename}'
+_STROTSS_DEFAULTS = ['block1_conv1',
+                     'block1_conv2',
+                     'block2_conv1',
+                     'block2_conv2',
+                     'block3_conv1',
+                     'block3_conv2',
+                     'block3_conv3',
+                     'block4_conv3',
+                     'block5_conv3']
 
 class VGG(tf.Module):
-    def __init__(self, layers: list,
-                 vgg_type: str='19',
+    """feature extractor."""
+
+    def __init__(self, layers: Optional[list] = None,
+                 vgg_type: str='16',
                  use_keras_weight: bool = False,
-                 **kwargs):
-        mean = kwargs.pop('mean', [0.485, 0.456, 0.406])
-        std = kwargs.pop('std', [0.229, 0.224, 0.225])
-        super().__init__(**kwargs)
+                 name: Optional[str] = None):
         vgg_type = str(vgg_type)
         assert vgg_type in ['16', '19']
+        layers = layers or _STROTSS_DEFAULTS
+
+        super().__init__(name = name)
 
         if not use_keras_weight:
             filename = f'vgg{vgg_type}_norm.h5'
-            if not tf.io.gfile.exists(f'./{filename}'):
-                _request_vgg_model(filename)
-            weight_path = f'./{filename}'
-            self.mean = tf.reshape(tf.convert_to_tensor(mean, dtype=tf.float32), (1, 1, 1, -1))
-            self.std = tf.reshape(tf.convert_to_tensor(std, dtype=tf.float32), (1, 1, 1, -1))
+            url = _URL.format(filename=filename)
+            weight_path = tf.keras.utils.get_file(filename, url)
+            self.mean = tf.reshape(tf.convert_to_tensor([0.485, 0.456, 0.406], dtype=tf.float32), (1, 1, 1, -1))
+            self.std = tf.reshape(tf.convert_to_tensor([0.229, 0.224, 0.225], dtype=tf.float32), (1, 1, 1, -1))
         else:
             weight_path = 'imagenet'
             self.preprocess = lambda x: getattr(tf.keras.applications, f'vgg{vgg_type}').preprocess_input(x*255)
@@ -55,5 +51,5 @@ class VGG(tf.Module):
         return (inputs - self.mean) / self.std
 
     @tf.Module.with_name_scope
-    def __call__(self, inputs):
+    def __call__(self, inputs: tf.Tensor) -> tf.Tensor:
         return self.net(self.preprocess(inputs))
